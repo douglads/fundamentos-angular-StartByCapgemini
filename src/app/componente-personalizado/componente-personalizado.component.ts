7533d74cb63169bd4bbf9c647d7633f997cc14fa
Douglads <douglasfm1995@gmail.com>
2023-01-08 10:51:56 -0300

    Aula 4-G: Personalizando componentes

--- src/app/componente-personalizado/componente-personalizado.component.ts
+++ src/app/componente-personalizado/componente-personalizado.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-componente-personalizado',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./componente-personalizado.component.css']
 })
 export class ComponentePersonalizadoComponent {
-
+  @Input() nome = "";
+  @Input() sobrenome = "";
 }
